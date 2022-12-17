d3ebbdbec7d7084c517ce67e16413dd4f8df57fe
GerardCoquard <gerard.coquard.franco@gmail.com>
2022-12-17 12:07:33 +0100

    aun error de packages

---
+++ Assets/InputScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
+using UnityEngine.UI;
+
+
+public class InputScript : MonoBehaviour
+{
+    public void OnMove()
+    {
+        
+    }
+}
